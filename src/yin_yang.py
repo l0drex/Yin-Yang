5c8c842baadcaac57a19cd1c252f78afdd0d1dd0
l0drex <l0drex@pm.me>
2020-11-26 19:49:20 +0100

    Further simplifications

--- src/yin_yang.py
+++ src/yin_yang.py
@@ -9,88 +9,57 @@ license: MIT
 """
 
 import datetime
-import os
-import pwd
 import threading
 import time
 
 from src import config
 
-# aliases for path to use later on
-user = pwd.getpwuid(os.getuid())[0]
-path = "/home/" + user + "/.config/"
 
 configParser = config.ConfigParser(-1)
 plugins = config.PLUGINS
 
-terminate = False
-
-
-class Switch(threading.Thread):
-    def __init__(self, thread_id):
-        threading.Thread.__init__(self)
-        self.thread_id = thread_id
-
-    def set_mode(self, dark: bool):
-        for p in plugins:
-            p.set_mode(dark)
-
 
 class Daemon(threading.Thread):
+    terminate = False
+
     def __init__(self, thread_id):
         threading.Thread.__init__(self)
         self.thread_id = thread_id
+
+        self.dark_mode: bool = configParser.get('dark_mode')
 
     def run(self):
         while True:
-
-            if terminate:
+            if self.terminate:
                 configParser.update("running", False)
                 break
 
-            if not config.get('mode') == config.Modes.scheduled.value:
+            if configParser.get('mode') == config.Modes.manual.value:
                 configParser.update("running", False)
                 break
 
-            if should_be_dark():
-                if configParser.get("dark_mode"):
-                    time.sleep(30)
-                    continue
-                else:
-                    switch_to_light()
-            else:
-                if not configParser.get("dark_mode"):
-                    time.sleep(30)
-                    continue
-                else:
-                    switch_to_dark()
+            # check if dark mode should be enabled
+            dark: bool = should_be_dark()
+
+            # switch if necessary
+            if dark != self.dark_mode:
+                self.dark_mode = dark
+                configParser.update('dark_mode', dark)
+                for p in plugins:
+                    p.set_mode(dark)
 
             time.sleep(30)
 
 
-def switch_to_light():
-    yang = Yang(1)
-    yang.start()
-    configParser.update("dark_mode", False)
-    yang.join()
-
-
-def switch_to_dark():
-    yin = Yin(2)
-    yin.start()
-    configParser.update("dark_mode", True)
-    yin.join()
-
-
 def start_daemon():
-    daemon = Daemon(3)
+    daemon = Daemon(1)
     daemon.start()
 
 
 def should_be_dark():
-    # desc: return if the Theme should be light
-    # returns: True if it should be light
-    # returns: False if the theme should be dark
+    """
+    Determines whether dark mode should be enabled or not
+    """
 
     d_hour = int(configParser.get("switch_To_Dark").split(":")[0])
     d_minute = int(configParser.get("switch_To_Dark").split(":")[1])
@@ -99,7 +68,7 @@ def should_be_dark():
     hour = datetime.datetime.now().time().hour
     minute = datetime.datetime.now().time().minute
 
-    if hour >= l_hour and hour < d_hour:
+    if l_hour <= hour < d_hour:
         return hour == l_hour and minute <= l_minute
     else:
         return not (hour == d_hour and minute <= d_minute)
@@ -107,7 +76,5 @@ def should_be_dark():
 
 def toggle_theme():
     """Switch themes"""
-    if configParser.get("dark_mode"):
-        switch_to_dark()
-    else:
-        switch_to_light()
+    for p in plugins:
+        p.set_mode(not configParser.get('dark_mode'))
