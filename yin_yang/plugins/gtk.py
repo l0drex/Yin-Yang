9c3676ab1eb1de035f661fe977918d9d6b053ffa
l0drex <l0drex@pm.me>
2021-02-15 13:46:31 +0100

    Changed gtk to proper strategy pattern

--- yin_yang/plugins/gtk.py
+++ yin_yang/plugins/gtk.py
@@ -16,41 +16,40 @@ class Gtk(Plugin):
     theme_dark = ''
     theme_bright = ''
 
-    # these subclasses are called instead of the actual class to change behaviour in different environments
-    # while keeping consistency with other plugins
-    # TODO use strategy pattern here
-
-    class Standard(Plugin):
-        # TODO set default theme names
-        theme_dark = ''
-        theme_bright = ''
-
-        def set_theme(self, theme: str):
-            subprocess.run(
-                ["gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", theme])  # Applications theme
-
-    class Kde(Plugin):
-        theme_bright = 'Breeze'
-        theme_dark = 'Breeze'
-
-        def set_theme(self, theme: str):
-            with open(path + "/settings.ini", "r") as file:
-                # search for the theme section and change it
-                current_theme = re.findall(
-                    'gtk-theme-name=[A-z -]*', str(file.readlines()))[0][:-2]
-                inplace_change(path + "/settings.ini", current_theme, "gtk-theme-name=" + theme)
-
     def __init__(self, theme_dark: Optional[str] = None, theme_bright: Optional[str] = None):
         super().__init__(theme_dark, theme_bright)
 
-        self.mode = self.Standard()
+        self._strategy = Standard()
 
         # FIXME themes are not set correctly
-        self.theme_dark = self.mode.theme_dark
-        self.theme_bright = self.mode.theme_bright
+        self.theme_bright = self._strategy.theme_bright
+        self.theme_dark = self._strategy.theme_dark
 
     def use_kde(self):
-        self.mode = self.Kde()
+        self._strategy = Kde()
 
     def set_theme(self, theme: str):
-        self.mode.set_theme(theme)
+        self._strategy.set_theme(theme)
+
+
+class Standard(Plugin):
+    # TODO set default theme names
+    theme_dark = ''
+    theme_bright = ''
+
+    def set_theme(self, theme: str):
+        # noinspection SpellCheckingInspection
+        subprocess.run(
+            ["gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", theme])  # Applications theme
+
+
+class Kde(Plugin):
+    theme_bright = 'Breeze'
+    theme_dark = 'Breeze'
+
+    def set_theme(self, theme: str):
+        with open(path + "/settings.ini", "r") as file:
+            # search for the theme section and change it
+            current_theme = re.findall(
+                'gtk-theme-name=[A-z -]*', str(file.readlines()))[0][:-2]
+            inplace_change(path + "/settings.ini", current_theme, "gtk-theme-name=" + theme)
