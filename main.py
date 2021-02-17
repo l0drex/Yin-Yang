65442b56d8f2197027e15b2909f253f4dac1aec0
l0drex <l0drex@pm.me>
2021-02-17 13:14:58 +0100

    Remove schedule arg

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ from PyQt5 import QtCore
 
 from yin_yang import yin_yang
 from yin_yang.ui import gui
-from yin_yang.config import Modes, config
+from yin_yang.config import config
 
 
 # using ArgumentParser for parsing arguments
 parser = ArgumentParser()
 parser.add_argument("-t", "--toggle",
-                    help="toggles Yin-Yang",
-                    action="store_true")
-parser.add_argument("-s", "--schedule",
-                    help="schedule theme toggle, starts daemon in bg",
+                    help="Changes to dark or light if needed",
                     action="store_true")
 parser.add_argument('-d', '--debugging',
                     help='enables debugging mode',
@@ -30,17 +27,7 @@ def main():
 
     config.debugging = args.debugging
 
-    # set settings via terminal
-    if args.schedule:
-        mode = config.get("mode")
-        if mode == Modes.manual.value:
-            print("looks like you did not specified a time")
-            print("You can use the gui with yin-yang -gui")
-            print("Or edit the config found in ~/.config/yin_yang/yin_yang.json")
-            print("You need to set schedule to True and edit the time to toggles")
-        else:
-            print(f"Using mode {mode}")
-    elif args.toggle:
+    if args.toggle:
         # toggle theme manually
         yin_yang.toggle_theme()
     else:
