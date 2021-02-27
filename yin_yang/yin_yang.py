e827596395d668b8a044796b489a0e6c1aa03281
l0drex <l0drex@pm.me>
2021-02-27 18:04:24 +0100

    Added daemon name

--- yin_yang/yin_yang.py
+++ yin_yang/yin_yang.py
@@ -30,8 +30,7 @@ class Setter:
             return
 
         logger.info(f'Switching to {"dark" if dark else "light"} mode.')
-        config.update('dark_mode', dark)
-        self.dark_mode = config.get('dark_mode')
+        self.dark_mode = config.update('dark_mode', dark)
         for p in PLUGINS:
             if config.get('enabled', plugin=p.name):
                 p.set_mode(dark)
@@ -49,6 +48,7 @@ class Daemon(threading.Thread):
         threading.Thread.__init__(self)
         self.thread_id = thread_id
         self.setter = Setter()
+        self.name = 'Yin-Yang'
 
     def run(self):
         while True:
