4456c1655dec374786f5fbb0abb2b683033f7457
l0drex <l0drex@pm.me>
2021-02-27 18:03:56 +0100

    Respect previous settings

--- tests/setter_test.py
+++ tests/setter_test.py
@@ -8,14 +8,17 @@ class SetterTest(unittest.TestCase):
     def test_update_dark_value(self):
         setter = Setter()
 
+        mode_used = config.get('mode')
         config.update('mode', Modes.manual.value)
-        mode_currently_used = config.get('dark_mode')
+        dark_mode_active = config.get('dark_mode')
 
-        setter.set_mode(not mode_currently_used)
-        self.assertEqual(not mode_currently_used, config.get('dark_mode'))
+        setter.set_mode(not dark_mode_active)
+        self.assertEqual(not dark_mode_active, config.get('dark_mode'))
 
-        setter.set_mode(mode_currently_used)
-        self.assertEqual(mode_currently_used, config.get('dark_mode'))
+        setter.set_mode(dark_mode_active)
+        self.assertEqual(dark_mode_active, config.get('dark_mode'))
+
+        config.update('mode', mode_used)
 
 
 if __name__ == '__main__':
