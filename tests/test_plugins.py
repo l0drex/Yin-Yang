4e3a1cb7b920b3b18f8300ee5233e6719f5b2a2b
l0drex <l0drex@pm.me>
2021-02-28 19:04:32 +0100

    Fix unsuccessful test when application not installed

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -15,14 +15,14 @@ class PluginsTest(unittest.TestCase):
                 self.assertTrue(pl.theme_dark is not None and pl.theme_bright is not None,
                                 'No default theme is specified. ' +
                                 'If your plugin does not support any default themes, use empty strings.')
-                self.assertIsInstance(pl.get_themes_available(), dict,
-                                      'Available themes always should be a dict.')
 
     def test_set_theme_works(self):
         for pl in PLUGINS:
             with self.subTest(plugin=pl.name):
                 if config.get('enabled', pl.name):
                     pl.set_mode(config.get('dark_mode'))
+                    self.assertIsInstance(pl.get_themes_available(), dict,
+                                          'Available themes always should be a dict.')
 
 
 if __name__ == '__main__':
