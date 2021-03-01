54a5add7865f9fb451fded8d1b5a977cc6a49010
l0drex <l0drex@pm.me>
2021-03-01 12:06:37 +0100

    Added strategy pattern to listener and added a native one

--- yin_yang/listener.py
+++ yin_yang/listener.py
@@ -1,34 +1,76 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+
+import time
+from abc import ABC, abstractmethod
 
 import dbus
-import subprocess
 from gi.repository import GLib
 from dbus.mainloop.glib import DBusGMainLoop
 
-# source: https://github.com/FedeDP/Clight/wiki/DE-Automation
-old_time = -1  # 0 -> day, 1 -> night
+from yin_yang.checker import Checker
+from yin_yang.config import config, Modes
+from yin_yang.yin_yang import set_mode
 
 
-def handle_time_change(*args):
-    global old_time
-    if 'DayTime' in args[1] and old_time != args[1]['DayTime']:
-        if args[1]['DayTime'] == 1:
-            # noinspection SpellCheckingInspection
-            subprocess.Popen(["lookandfeeltool", "-a", "org.kde.breezedark.desktop"])
-        elif args[1]['DayTime'] == 0:
-            # noinspection SpellCheckingInspection
-            subprocess.Popen(["lookandfeeltool", "-a", "org.kde.breeze.desktop"])
-        old_time = args[1]['DayTime']
+class Listener:
+    def __init__(self, listener):
+        if listener == 'native':
+            self._mode = Native()
+        elif listener == 'clight':
+            self._mode = Clight()
+
+    def run(self):
+        self._mode.run()
 
 
-DBusGMainLoop(set_as_default=True)
-bus = dbus.SessionBus()
-# noinspection SpellCheckingInspection
-bus.add_signal_receiver(
-    handle_time_change,
-    'PropertiesChanged',
-    'org.freedesktop.DBus.Properties',
-    path='/org/clight/clight'
-)
+class Mode(ABC):
+    terminate = False
 
-loop = GLib.MainLoop().run()
+    @abstractmethod
+    def run(self):
+        raise NotImplementedError('Method is not implemented.')
+
+
+class Native(Mode):
+    def __init__(self):
+        super().__init__()
+        self._checker = Checker(config.get('mode'))
+
+    def run(self):
+        while True:
+            if self.terminate or config.get('mode') == Modes.manual.value:
+                config.update("running", False)
+                config.write()
+                break
+
+            # check if dark mode should be enabled and switch if necessary
+            dark_mode = self._checker.should_be_dark()
+            if config.get('dark_mode') != dark_mode:
+                set_mode(dark_mode)
+
+            time.sleep(60)
+
+
+class Clight(Mode):
+    # source: https://github.com/FedeDP/Clight/wiki/DE-Automation
+    _old_time = -1  # 0 -> day, 1 -> night
+
+    def __init__(self):
+        super().__init__()
+        DBusGMainLoop(set_as_default=True)
+        bus = dbus.SessionBus()
+        # noinspection SpellCheckingInspection
+        bus.add_signal_receiver(
+            self.handle_time_change,
+            'PropertiesChanged',
+            'org.freedesktop.DBus.Properties',
+            path='/org/clight/clight'
+        )
+
+    def handle_time_change(self, *args):
+        if 'DayTime' in args[1] and self._old_time != args[1]['DayTime']:
+            set_mode(bool(args[1]['DayTime']))
+            self._old_time = args[1]['DayTime']
+
+    def run(self):
+        GLib.MainLoop().run()
