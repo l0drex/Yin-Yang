89b8edf673bed69231981cc38344844982b18a21
l0drex <l0drex@pm.me>
2021-03-07 20:33:09 +0100

    Types, Fixes, Improvements

--- communicate.py
+++ communicate.py
@@ -9,32 +9,35 @@ from pathlib import Path
 
 from yin_yang.config import config, Modes
 
-logging.basicConfig(filename=str(Path.home()) + '/.local/share/yin_yang.log', level=logging.WARNING,
+logging.basicConfig(filename=str(Path.home()) + '/.local/share/yin_yang.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s - %(name)s: %(message)s')
 logger = logging.getLogger(__name__)
 
 
-def parse_time(time: str):
+def parse_time(time: str) -> tuple[int, int]:
     hour = int(time.split(":")[0])
     minute = int(time.split(":")[1])
-    return [hour, minute]
+    return hour, minute
 
 
 def create_message() -> dict:
+    logger.debug('Building message')
     theme_light = config.get("light_theme", "firefox")
     theme_dark = config.get("dark_theme", "firefox")
-    theme_active = theme_dark if config.get('dark_mode') else theme_light
-    return {
+    theme_active: str = theme_dark if config.get('dark_mode') else theme_light
+    message = {
         'schedule': config.get("mode") != Modes.manual.value,
         'theme_dark': theme_dark,
         'theme_light': theme_light,
         'theme_active': theme_active,
-        'time_day': parse_time(config.get("switchToLight")),
-        'time_night': parse_time(config.get("switchToDark"))
+        'time_day': parse_time(config.get("switch_To_Light")),
+        'time_night': parse_time(config.get("switch_To_Dark"))
     }
 
+    return message
 
-def encode_message(message_content: dict):
+
+def encode_message(message_content: dict) -> dict[str, bytes]:
     """
     Encode a message for transmission, given its content.
     :param message_content: a message
@@ -44,18 +47,21 @@ def encode_message(message_content: dict):
     # use struct.pack("10s", bytes)
     # to pack a string of the length of 10 characters
 
-    return {'length': encoded_length,
-            'content': struct.pack(str(len(encoded_content))+"s",
-                                   encoded_content)}
+    encoded_message = {
+        'length': encoded_length,
+        'content': struct.pack(str(len(encoded_content)) + "s",
+                               encoded_content)}
+    logger.debug('Encoded message with length ' + str(len(encoded_content)))
+    return encoded_message
 
 
 # Send an encoded message to stdout.
-def send_message(encoded_message):
+def send_message(encoded_message: dict[str, bytes]):
     """
     Send a message.
     :param encoded_message: message as json
     """
-    logger.debug('Sending message: ' + encoded_message)
+    logger.debug('Sending message')
     sys.stdout.buffer.write(encoded_message['length'])
     sys.stdout.buffer.write(encoded_message['content'])
     sys.stdout.buffer.flush()
@@ -79,6 +85,4 @@ while True:
         logger.debug('Message received: ' + message_received)
 
     if message_received == 'GetSettings':
-        message_send = create_message()
-
-        send_message(encode_message(message_send))
+        send_message(encode_message(create_message()))
