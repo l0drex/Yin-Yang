f0b995ffa087e3bf7d789ca1a4d335596549d665
l0drex <l0drex@pm.me>
2021-03-10 13:48:52 +0100

    Implement get_available_themes()

--- yin_yang/plugins/firefox.py
+++ yin_yang/plugins/firefox.py
@@ -1,4 +1,10 @@
+import json
+from pathlib import Path
+
 from yin_yang.plugins.plugin import Plugin
+
+# TODO more general please
+path = str(Path.home()) + '/.mozilla/firefox/sooi88to.default-release/extensions.json'
 
 
 class Firefox(Plugin):
@@ -8,3 +14,14 @@ class Firefox(Plugin):
 
     def set_theme(self, theme: str):
         pass
+
+    def get_themes_available(self) -> dict[str, str]:
+        themes: dict[str, str] = {}
+
+        with open(path, 'r') as file:
+            content = json.load(file)
+            for addon in content['addons']:
+                if addon['type'] == 'theme':
+                    themes[addon['id']] = addon['defaultLocale']['name']
+
+        return themes
