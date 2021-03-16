5e2836f5e96db8f963ad3549b172f5c5455a4c5c
l0drex <l0drex@pm.me>
2021-03-16 15:22:51 +0100

    Fixed some translations

--- resources/yin_yang.de.ts
+++ resources/yin_yang.de.ts
@@ -5,7 +5,7 @@
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="232"/>
         <source>Settings</source>
-        <translation type="unfinished">Einstellungen</translation>
+        <translation>Einstellungen</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="222"/>
@@ -15,47 +15,47 @@
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="223"/>
         <source>Custom Schedule</source>
-        <translation type="unfinished">benutzdefinierter Zeitraum</translation>
+        <translation>Benutzerdefinierter Zeitraum</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="224"/>
         <source>Sunset to Sunrise</source>
-        <translation type="unfinished">Sonnenaufgang bis Sonnenuntergang</translation>
+        <translation>Sonnenaufgang bis Sonnenuntergang</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="225"/>
         <source>Light:</source>
-        <translation type="unfinished">Hell:</translation>
+        <translation>Hell:</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="226"/>
         <source>Dark:</source>
-        <translation type="unfinished">Dunkel:</translation>
+        <translation>Dunkel:</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="227"/>
         <source>Longitude:</source>
-        <translation type="unfinished">Längengrad:</translation>
+        <translation>Längengrad:</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="228"/>
         <source>Latitude:</source>
-        <translation type="unfinished">Breitengrad:</translation>
+        <translation>Breitengrad:</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="229"/>
         <source>update automatically</source>
-        <translation type="unfinished">Automatisch aktualisieren</translation>
+        <translation>Automatisch aktualisieren</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="230"/>
         <source>Make a sound when switching the theme</source>
-        <translation type="unfinished">Mache ein Geräusch, wenn das Thema geändert wird</translation>
+        <translation>Mache ein Geräusch, wenn das Thema geändert wird</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/main_window.py" line="231"/>
         <source>Send a notification</source>
-        <translation type="unfinished">Sende eine Benachrichtigung</translation>
+        <translation>Sende eine Benachrichtigung</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/mainwindow.py" line="231"/>
@@ -70,32 +70,37 @@
     <message>
         <location filename="../yin_yang/ui/config_window.py" line="37"/>
         <source>You are using Version {config.version}. </source>
-        <translation type="unfinished">Sie benutzen Version {config.versionn}. </translation>
+        <translation type="obsolete">Sie benutzen Version {config.versionn}. </translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/config_window.py" line="37"/>
         <source>Yin-Yang is {&quot;not &quot; if not config.running else &quot;&quot;}running.</source>
-        <translation type="unfinished">Yin-Yang läuft {&quot;nicht&quot; if not config.running else &quot;&quot;}.</translation>
+        <translation>Yin-Yang läuft zurzeit {&quot;nicht&quot; if not config.running else &quot;&quot;} im Hintergrund.</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/config_window.py" line="131"/>
         <source>Dark mode will be active between {time_dark.strftime(&quot;%H:%M&quot;)} and {time_light.strftime(&quot;%H:%M&quot;)}.</source>
-        <translation type="unfinished">Dunkler Modus wird aktiviert zwischen {time_dark.strftime(&quot;%H:%M)} und {time_light.strftime(&quot;%H:%M&quot;)}.</translation>
+        <translation>Dunkler Modus wird zwischen {time_dark.strftime(&quot;%H:%M)} und {time_light.strftime(&quot;%H:%M&quot;)} aktiviert.</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/config_window.py" line="222"/>
         <source>Open Wallpaper {&quot;dark&quot; if dark else &quot;light&quot;}</source>
-        <translation type="unfinished">Öffne {&quot;dunklen&quot; if dark else &quot;hellen&quot;} Hintergrund</translation>
+        <translation>Öffne {&quot;dunklen&quot; if dark else &quot;hellen&quot;} Hintergrund</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/config_window.py" line="252"/>
         <source>The settings have been modified. Do you want to save them?</source>
-        <translation type="unfinished">Die Einstellungen wurden geändert. Möchten Sie sie speichern?</translation>
+        <translation>Die Einstellungen wurden geändert. Möchten Sie sie speichern?</translation>
     </message>
     <message>
         <location filename="../yin_yang/ui/config_window.py" line="253"/>
         <source>Unsaved changes</source>
-        <translation type="unfinished">Ungespeicherte Änderungen</translation>
+        <translation>Ungespeicherte Änderungen</translation>
+    </message>
+    <message>
+        <location filename="../yin_yang/ui/config_window.py" line="37"/>
+        <source>You are using version {config.version}. </source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
