1bebf60d5b30bac1961e83ae0badd5ff1cf68e38
l0drex <l0drex@pm.me>
2021-03-17 15:24:15 +0100

    Implemented get_available_themes in kvantum

--- yin_yang/plugins/kvantum.py
+++ yin_yang/plugins/kvantum.py
@@ -1,15 +1,35 @@
 import subprocess
+from os import listdir
+from os.path import join, isdir
+
 from yin_yang.plugins.plugin import Plugin
+
+
+def get_files_in_dir(path: str):
+    # copied from https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+    return [f for f in listdir(path) if isdir(join(path, f))]
+
 
 
 class Kvantum(Plugin):
     name = 'Kvantum'
     # TODO set default themes
-    theme_bright = ''
-    theme_dark = ''
+    theme_bright = 'KvFlatLight'
+    theme_dark = 'KvFlat'
 
     def set_theme(self, theme: str):
         # uses a kvantum manager cli to switch to a light theme
         print("Kvantum Light theme:", theme)
         # noinspection SpellCheckingInspection
         subprocess.run(["kvantummanager", "--set", theme])
+
+    def get_themes_available(self) -> dict[str, str]:
+        themes = get_files_in_dir('/usr/share/Kvantum')
+        themes_dict: dict = {}
+        assert len(themes) > 0, 'No themes were found'
+
+        themes.sort()
+        for theme in themes:
+            themes_dict[theme] = theme
+
+        return themes_dict
