3a98dfdd9d53d1741cefaf958d1c9856e9e0c8e5
l0drex <l0drex@pm.me>
2021-04-03 15:04:58 +0200

    Improved installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,74 +2,46 @@
 
 set -euo pipefail
 
-HOME=${HOME}
-
 if test ${EUID} -ne 0; then
-    echo enter password in order to install Yin-Yang correctly
+    echo "Enter password in order to install Yin-Yang correctly."
     exec sudo su -c "${0} ${HOME}"
     exit 0
 fi
 
-echo "removing old Yin-Yang files if they exist"
+echo
+echo "Removing old Yin-Yang files if they exist."
 ./scripts/uninstall.sh "${HOME}"
 
-echo "Installing Yin-Yang ..."
-echo ""
-echo "Checking for QT dependencies"
-echo ""
-#checking python dependencies
+echo
+echo "Installing dependencies..."
 pip3 install qtpy
 pip3 install pyqt5
 pip3 install suntime
-echo ""
-echo "Checking and creating correct folders ..."
-#check if /opt/ directory exists else create
+
+echo
+echo "Creating directories..."
+# check if /opt/ directory exists else create
 if [ ! -d /opt/ ]; then
     mkdir -p /opt/
 fi
-#check if /opt/ directory exists else create
 if [ ! -d /opt/yin-yang/ ]; then
     mkdir -p /opt/yin-yang/
 fi
-if [ ! -d "${HOME}/.local/share/applications/" ]; then
-    mkdir -p "${HOME}/.local/share/applications/"
+if [ ! -d ${HOME}/.local/share/applications/ ]; then
+    mkdir -p ${HOME}/.local/share/applications/
 fi
-echo ""
-echo "done"
-echo ""
-echo "Installing yin-yang for command line usage"
+
+echo
+echo "Installing Yin-Yang..."
 # copy files
 cp -r ./* /opt/yin-yang/
 # copy terminal executive
 cp ./scripts/yin-yang /usr/bin/
 chmod +x /usr/bin/yin-yang
-echo "Creating .desktop file for native environment execution"
-# create .desktop file
-cat > "${HOME}/.local/share/applications/Yin-Yang.desktop" <<EOF
-[Desktop Entry]
-# The type as listed above
-Type=Application
-# The version of the desktop entry specification to which this file complies
-Version=1.4
-# The name of the application
-Name=Yin & Yang
-# Generic name of the application, for example "Web Browser"
-GenericName=Theme Switcher
-# A comment which can/will be used as a tooltip
-Comment=Auto dark mode for KDE and VSCode
-# The path to the folder in which the executable is run
-Path=/opt/yin-yang
-# The executable of the application, possibly with arguments.
-Exec=env QT_AUTO_SCREEN_SCALE_FACTOR=1 sh /usr/bin/yin-yang
-# The name of the icon that will be used to display this entry
-Icon=/opt/yin-yang/resources/icon.svg
-# Describes whether this application needs to be run in a terminal or not
-Terminal=false
-# Describes the categories in which this entry should be shown
-Categories=Utility; System; Settings;
-# A list of strings which may be used in addition to other metadata to describe this entry
-Keywords=night;dark;day;bright;color;theme;
-EOF
+
+echo
+echo "Creating .desktop file for native environment execution..."
+cp ./resources/Yin-Yang.desktop ${HOME}/.local/share/applications/Yin-Yang.desktop
 
 cat << "EOF"
  __     ___          __     __
@@ -81,9 +53,10 @@ cat << "EOF"
                                         __/ |
                                        |___/
 EOF
-echo ""
-echo "Yin-Yang brings automatic dark mode for KDE and VSCode"
-echo ""
+echo
+echo "Welome to Yin-Yang! Activate auto dark mode for various applications by starting the application."
+echo
+
 cat << "EOF"
        _..oo8"""Y8b.._
      .88888888o.    "Yb.
@@ -100,7 +73,6 @@ cat << "EOF"
        `""Y888boodP""'
 
 EOF
-echo ""
-echo ""
-echo "checkout https://github.com/daehruoydeef/Yin-Yang for help"
-echo "Yin-Yang is now installed"
+
+echo "Checkout https://github.com/daehruoydeef/Yin-Yang if your need help."
+echo "Yin-Yang is now installed."
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# this script will uninstall Yin-Yang and will also delete its config files
+# This script will uninstall Yin-Yang and will also delete its config files
 
 set -euo pipefail
 
-home=${HOME}
-
 # check, if sudo
 if test ${EUID} -ne 0; then
-    echo enter password in order to install Yin-Yang correctly
+    echo "Enter password in order to install Yin-Yang correctly"
+    echo
     exec sudo su -c "${0} ${HOME}"
     exit 0
 fi
 
-echo "Removing config and .desktop file"
-rm -rf "${home}"/.local/share/applications/Yin-Yang.desktop
-rm -rf "${home}"/.config/yin_yang
+echo "Removing config and .desktop file..."
+rm -rf ${HOME}/.local/share/applications/Yin-Yang.desktop
+rm -rf ${HOME}/.config/yin_yang
 
 echo "Removing program and terminal execution"
 rm -rf /opt/yin-yang /usr/bin/yin-yang.sh
 
-echo Yin-Yang uninstalled succesfully
-echo have a nice day ...
+echo
+echo Yin-Yang uninstalled succesfully.
