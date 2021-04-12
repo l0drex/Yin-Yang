ecf44f8fd551a83b47baf9edfdb02ffa8faee738
l0drex <l0drex@pm.me>
2021-04-12 17:28:03 +0200

    Fix paths

--- scripts/build_ui.sh
+++ scripts/build_ui.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # resource file
-pyrcc5 ./resources.qrc -o ./resources_rc.py
+pyrcc5 ../resources.qrc -o ../resources_rc.py
 # ui file from qt designer
-pyuic5 -o ./yin_yang/ui/main_window.py ./designer/main_window.ui
+pyuic5 -o ../yin_yang/ui/main_window.py ../designer/main_window.ui
 # translation files
-pylupdate5 yin-yang.pro
+pylupdate5 ../yin-yang.pro
